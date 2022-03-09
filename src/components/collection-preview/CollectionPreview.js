8d6f4c3b7ed4248c8dd898d62a39a48b66be903b
willitech <williamnnamani@yahoo.com>
2022-03-09 18:08:53 +0100

    create a collection preview page and a collection items

--- src/components/collection-preview/CollectionPreview.js
+++ src/components/collection-preview/CollectionPreview.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './CollectionPreview.scss';
+import CollectionItem from '../collection-item/CollectionItem';
 
 const CollectionPreview = ({title, items}) => {
     return(
@@ -6,8 +8,8 @@ const CollectionPreview = ({title, items}) => {
            <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
               {
-                 items.filter((item,idx) => idx <4).map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                 items.filter((item,idx) => idx <4).map(({ id, ...OtherItemProps }) => (
+                    <CollectionItem key={id} {...OtherItemProps} />
                  ))
               }
               
